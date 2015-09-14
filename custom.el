2467bad7907eab9da85e18fa6eb5149f814af7cc
l3msh0 <s-matsumoto@infiniteloop.co.jp>
2015-09-14 13:11:59 +0900

    Change web-mode offset

--- custom.el
+++ custom.el
@@ -42,6 +42,10 @@
  '(tab-width 4)
  '(tramp-default-method "rsync")
  '(twittering-use-master-password t)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-markup-indent-offset 2)
+ '(web-mode-sql-indent-offset 2)
  '(whitespace-style (quote (face trailing tabs tab-mark))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
