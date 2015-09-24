9c8f2f58b0986b347c165314d15a6369a0ae9319
l3msh0 <s-matsumoto@infiniteloop.co.jp>
2015-09-24 10:39:54 +0900

    Add settings for twittering-mode

--- inits/50_twitter.el
+++ inits/50_twitter.el
@@ -1,6 +1,13 @@
 (el-get-bundle twittering-mode)
 (use-package twittering-mode
+  :commands (twit)
   :config
   (add-hook 'twittering-mode-hook
             (lambda ()
-              (twittering-icon-mode 1))))
+              (twittering-icon-mode 1)))
+  (fmakunbound 'twittering-update-lambda)
+  (fmakunbound 'twittering-jojo-mode)
+  (bind-keys :map twittering-mode-map
+             ("f" . twittering-switch-to-next-timeline)
+             ("p" . twittering-switch-to-previous-timeline)
+             ("F" . twittering-favorite)))
