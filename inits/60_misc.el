db42513f7abe4155d99e71fcd31dcc63a25cb094
l3msh0 <s-matsumoto@infiniteloop.co.jp>
2015-09-25 10:31:42 +0900

    orgtbl

--- inits/60_misc.el
+++ inits/60_misc.el
@@ -9,3 +9,17 @@
 ;; Libraries
 (el-get-bundle deferred)
 (el-get-bundle json)
+
+
+(require 'org-table)
+
+(defun cleanup-org-tables ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (search-forward "-+-" nil t) (replace-match "-|-"))
+    ))
+
+(add-hook 'markdown-mode-hook 'orgtbl-mode)
+(add-hook 'markdown-mode-hook
+          (lambda()
+            (add-hook 'before-save-hook 'cleanup-org-tables  nil 'make-it-local)))
