c6c242ad160f980f08f70b4e6fa2fb9bf8ef6fd0
l3msh0 <L3msh0@gmail.com>
2016-01-21 11:37:46 +0900

    Remove ido-migemo

--- inits/35_ido.el
+++
@@ -1,6 +0,0 @@
-(el-get-bundle elpa:ido-migemo)
-(use-package ido-migemo
-  :config
-  (ido-migemo-mode 1)
-  (setq ido-migemo-exclude-command-list '(smex ido-switch-buffer))
-  (setq ido-migemo-prompt-string "[MIGEMO] "))
