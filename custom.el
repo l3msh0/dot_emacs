8c65661c0ad8beb8dadc221570432af140c7cc1e
l3msh0 <L3msh0@gmail.com>
2016-03-14 18:10:40 +0900

    Extend twittering-timer-interval

--- custom.el
+++ custom.el
@@ -57,7 +57,7 @@
  '(twittering-convert-fix-size 32)
  '(twittering-default-show-replied-tweets 3)
  '(twittering-status-format "%i %p%s %t %@%R")
- '(twittering-timer-interval 120)
+ '(twittering-timer-interval 180)
  '(twittering-use-master-password t)
  '(web-mode-code-indent-offset 2)
  '(web-mode-css-indent-offset 2)
