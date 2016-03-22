d86fb5361899c87a3ff96fce927093ca448df9b4
l3msh0 <L3msh0@gmail.com>
2016-03-22 20:44:54 +0900

    Rconfigure helm by customize

--- inits/35_helm.el
+++ inits/35_helm.el
@@ -7,10 +7,7 @@
          ("C-, r" . helm-resume))
   :config
   (bind-key "C-h" 'delete-backward-char helm-map)
-  (key-chord-define-global "bm" 'helm-bookmarks)
-  (add-to-list 'helm-completing-read-handlers-alist '(dired-do-copy . nil))
-  (add-to-list 'helm-completing-read-handlers-alist '(dired-do-rename . nil))
-  (add-to-list 'helm-completing-read-handlers-alist '(dired-create-directory . nil)))
+  (key-chord-define-global "bm" 'helm-bookmarks))
 
 (use-package helm-files
   :defer t
