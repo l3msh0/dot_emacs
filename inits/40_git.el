8cb834736ad377a76512bd35cf6dda166a287db7
l3msh0 <l3msh0@gmail.com>
2016-03-23 21:36:39 +0900

    Add git-timemachine

--- inits/40_git.el
+++ inits/40_git.el
@@ -3,6 +3,10 @@
   :config
   (global-git-gutter-mode 1))
 
+(el-get-bundle git-timemachine)
+(use-package git-timemachine
+  :commands (git-timemachine))
+
 (el-get-bundle magit)
 (use-package magit
   :commands (magit-status)
